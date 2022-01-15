c6f31b52ba00dd8a91acbec3578f9f5c5c9c96a4
Davit <sargsyan.davittt@gmail.com>
2022-01-16 03:16:04 +0400

    fix

--- src/Services/ApiClient.ts
+++ src/Services/ApiClient.ts
@@ -1,21 +1,3 @@
-// export const catsApiServic = {
-//     baseUrl: "https://api.thecatapi.com/v1",
-  
-//     Req: (endPoint, config) => 
-//       fetch(`${catsApiServic.baseUrl}${endPoint}`, config),
-  
-//       getCate: async (page, category) => {
-//       const response = await catsApiServic
-//         .Req(`/images/search?limit=10&page=${page}&category_ids=${category}`)
-//         .then((res) => res.json());
-//       return response;
-//     },
-  
-//     getCategory: async () => {
-//       return await catsApiServic.Req("/categories").then((res) => res.json());
-//     }
-//   };
-
 export const baseUrl = "https://api.thecatapi.com/v1";
 
 const Request = async (endPoint: string, config?: any) => 
